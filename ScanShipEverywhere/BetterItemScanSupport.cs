0cac6b63786d0fb2d452877b4a7ce283f1e61a00
TheBlackEntity <36412486+TheBlackEntity@users.noreply.github.com>
2024-07-17 13:40:33 +0200

    Make values dynamic

--- ScanShipEverywhere/BetterItemScanSupport.cs
+++ ScanShipEverywhere/BetterItemScanSupport.cs
@@ -1,12 +1,32 @@
+using System;
+using System.Reflection;
 using BetterItemScan.Patches;
 using HarmonyLib;
 
 namespace ScanShipEverywhere;
 
 public static class BetterItemScanSupport {
-    public static void Initialize() {
-        var maxRangeField = AccessTools.DeclaredField(typeof(PlayerControllerBPatch_A), "maxDistance");
+    private static FieldInfo? _maxRangeField;
 
-        maxRangeField.SetValue(null, ScanShipEverywhere.configManager.GetMaxScanDistanceHardLimit());
+    public static void Initialize() {
+        SetValue();
+
+        ScanShipEverywhere.configManager.SubscribeToMaxScanDistanceHardLimit(OnValueChanged);
+    }
+
+    public static void Uninitialize() => ScanShipEverywhere.configManager.UnsubscribeFromMaxScanDistanceHardLimit(OnValueChanged);
+
+    private static void OnValueChanged(object sender, EventArgs e) => SetValue();
+
+    private static void SetValue() {
+        _maxRangeField ??= AccessTools.DeclaredField(typeof(PlayerControllerBPatch_A), "maxDistance");
+
+        if (_maxRangeField is null) {
+            ScanShipEverywhere.Logger.LogFatal(
+                "Couldn't find \"_maxRangeField\" field in BetterItemScan's \"PingScan\" PlayerControllerBPatch_A!'");
+            return;
+        }
+
+        _maxRangeField.SetValue(null, ScanShipEverywhere.configManager.GetMaxScanDistanceHardLimit());
     }
 }
--- ScanShipEverywhere/Patches/ScanTweaksPingScanPatch.cs
+++ ScanShipEverywhere/Patches/ScanTweaksPingScanPatch.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Reflection;
 using HarmonyLib;
 using ScanTweaks;
 
@@ -5,17 +7,36 @@ namespace ScanShipEverywhere.Patches;
 
 [HarmonyPatch(typeof(PingScan))]
 public static class ScanTweaksPingScanPatch {
+    private static FieldInfo? _rangeField;
+    private static PingScan? _pingScan;
+
     [HarmonyPatch("OnEnable")]
     [HarmonyPostfix]
     // ReSharper disable once InconsistentNaming
     private static void OnEnablePostfix(PingScan __instance) {
-        var rangeField = AccessTools.DeclaredField(typeof(PingScan), "_range");
+        _pingScan = __instance;
 
-        if (rangeField is null) {
+        SetValue();
+
+        ScanShipEverywhere.configManager.SubscribeToMaxScanDistanceHardLimit(OnValueChanged);
+    }
+
+    [HarmonyPatch("OnDisable")]
+    [HarmonyPostfix]
+    private static void OnDisablePostfix() => ScanShipEverywhere.configManager.UnsubscribeFromMaxScanDistanceHardLimit(OnValueChanged);
+
+    private static void OnValueChanged(object sender, EventArgs e) => SetValue();
+
+    private static void SetValue() {
+        if (_pingScan is null) return;
+
+        _rangeField ??= AccessTools.DeclaredField(typeof(PingScan), "_range");
+
+        if (_rangeField is null) {
             ScanShipEverywhere.Logger.LogFatal("Couldn't find \"_range\" field in ScanTweaks' \"PingScan\" type!'");
             return;
         }
 
-        rangeField.SetValue(__instance, ScanShipEverywhere.configManager.GetMaxScanDistanceHardLimit());
+        _rangeField.SetValue(_pingScan, ScanShipEverywhere.configManager.GetMaxScanDistanceHardLimit());
     }
 }
