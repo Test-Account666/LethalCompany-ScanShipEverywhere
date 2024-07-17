ed7f02d68d9de64bd48119e882dcc07f779edaf4
TheBlackEntity <36412486+TheBlackEntity@users.noreply.github.com>
2024-07-17 13:41:24 +0200

    Remove GeneralImprovements incompatibility message

--- ScanShipEverywhere/GeneralImprovementsSupport.cs
+++
@@ -1,6 +0,0 @@
-namespace ScanShipEverywhere;
-
-public static class GeneralImprovementsSupport {
-    public static bool IsFixPersonalScannerEnabled() =>
-        GeneralImprovements.Plugin.FixPersonalScanner.Value;
-}
